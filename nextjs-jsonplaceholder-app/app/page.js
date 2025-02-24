d39c63211953263a500da18f71340cf75328d5c1
jimmy <tul70511@temple.edu>
2025-02-24 16:14:03 -0500

    Create HomePage and fetch 10 posts from API to display

---
+++ nextjs-jsonplaceholder-app/app/page.js
@@ -0,0 +1,26 @@
+
+// Limited the post amount to 10
+async function fetch10posts() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+    const posts = await res.json();
+
+    return posts;
+  }
+
+
+  const HomePage = async () => {
+    const posts = await fetch10posts();
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+
+    export default HomePage;
