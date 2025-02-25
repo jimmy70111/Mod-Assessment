5faef6300197522c896c41dda293e7fb8493136f
jimmy <tul70511@temple.edu>
2025-02-24 23:45:32 -0500

    Added Tailwind CSS on post details page
    - Applied CSS changes for the title and body of individual posts
    - Improved and Added UI for the "Back to Homepage" button

--- nextjs-jsonplaceholder-app/app/Post_Details/page.js
+++ nextjs-jsonplaceholder-app/app/Post_Details/page.js
@@ -5,20 +5,28 @@ import { useSearchParams } from "next/navigation";
 import { fetching } from "./fetching"; 
 
 function Display() {
-    const router = useRouter(); // using router to navigate back to the homepage
+    const router = useRouter();
     const searchParams = useSearchParams();
     const id = searchParams.get("id"); 
     const content = fetching(id); 
 
-    if (!content) return <p> Loading...</p>;
+    if (!content) return (
+        <p className="text-center  text-gray-600">Loading...</p>
+    );
 
     return (
-        <div>
-            <h1>{content.title}</h1>
-            <p>{content.body}</p>
-            <button  onClick={() => router.push("/")} >
-                Go Back to Homepage
-            </button>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
+            <div className="max-w-lg bg-white p-6 rounded-lg shadow-1g">
+                <h1 className="text-2xl font-bold text-gray-800">{content.title}</h1>
+                <p className="mt-7 text-gray-700">{content.body}</p>
+                
+                <button  
+                    className="mt-10 px-6 py-2 bg-black text-white font-bold rounded-full shadow-md transition"
+                    onClick={() => router.push("/")}
+                >
+                     Back
+                </button>
+            </div>
         </div>
     );
 }
