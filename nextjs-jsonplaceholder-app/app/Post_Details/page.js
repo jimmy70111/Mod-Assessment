bf031c3d95d1e19869bd8016b6113c2cc2621f0a
jimmy <tul70511@temple.edu>
2025-02-25 15:08:52 -0500

    Added styling for homepage 10 posts and improved code spacing for better organization

--- nextjs-jsonplaceholder-app/app/Post_Details/page.js
+++ nextjs-jsonplaceholder-app/app/Post_Details/page.js
@@ -6,20 +6,21 @@ import { fetching } from "./fetching";
 
 function Display() {
     
-    const router = useRouter();
-    const searchParams = useSearchParams();
+    const router = useRouter(); // useRouter hook for back nagivation
+    const searchParams = useSearchParams(); // useSearchParams hook to get the id
     const id = searchParams.get("id"); 
-    const content = fetching(id); 
+    const content = fetching(id); // fetching post details from the apI
 
-    if (!content) return (
+   if (!content) return (
         <p className="text-center  text-gray-600">Loading...</p>
     );
 
+    
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
             <div className="max-w-lg bg-white p-6 rounded-lg shadow-1g">
                 <h1 className="text-2xl font-bold text-gray-800  text-center">{content.title}</h1>
-                <p className="mt-7 text-gray-700 text-center">{content.body}</p>
+                <p className="mt-7 text-gray-700 text-center font-serif">{content.body}</p>
                 
                 <button  
                     className="mt-10 px-6 py-2 bg-black text-white font-bold rounded-full shadow-md transition"
--- nextjs-jsonplaceholder-app/app/page.js
+++ nextjs-jsonplaceholder-app/app/page.js
@@ -1,30 +1,29 @@
 import Link from "next/link";
 import { fetchPosts } from "./Api/fetchPosts";
 
+const HomePage = async () => {
 
-  const HomePage = async () => {
-    const posts = await fetchPosts();
+    // fetch posts from api fucntion to get the firs 10 posts
+   const posts = await fetchPosts();
 
 
 
 
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <ul>
-
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-3g bg-white p-5 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold text-gray-800 text-center">10 Posts</h1>
+        {/* Pass post ID as a link to the Post_Details */}
+        <ul className="mt-4 text-xl text-blue-600 list-disc list-inside marker:text-black underline">
           {posts.map((post) => (
             <li key={post.id}>
-
-{/* Pass post ID as a link to the Post Details component */}
-               <Link href={`/Post_Details?id=${post.id}`}>
-                {post.title}
-              </Link>
+              <Link href={`/Post_Details?id=${post.id}`}>{post.title}</Link>
             </li>
           ))}
         </ul>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-    export default HomePage;
+export default HomePage;
