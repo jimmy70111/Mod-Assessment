b51b22dc02f961ed0278cfac67d37b9d862499bb
jimmy <tul70511@temple.edu>
2025-02-25 18:58:03 -0500

    Improve rendering performance on the detail page

--- nextjs-jsonplaceholder-app/app/Post_Details/page.js
+++ nextjs-jsonplaceholder-app/app/Post_Details/page.js
@@ -11,16 +11,13 @@ function Display() {
     const id = searchParams.get("id"); 
     const content = fetching(id); // fetching post details from the apI
 
-   if (!content) return (
-        <p className="text-center  text-white">Loading...</p>
-    );
 
     
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
             <div className="max-w-lg bg-white p-6 rounded-lg shadow-1g">
-                <h1 className="text-2xl font-bold text-gray-800  text-center">{content.title}</h1>
-                <p className="mt-7 text-gray-700 text-center font-serif">{content.body}</p>
+                <h1 className="text-2xl font-bold text-gray-800  text-center">{content?.title}</h1>
+                <p className="mt-7 text-gray-700 text-center font-serif">{content?.body}</p>
                 
                 <button  
                     className="mt-10 px-6 py-2 bg-black text-white font-bold rounded-full shadow-md transition"
