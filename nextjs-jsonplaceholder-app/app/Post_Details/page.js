2c9fe5cce6d6da8921c848d704a67e573a00e365
jimmy <tul70511@temple.edu>
2025-02-25 22:04:48 -0500

    Fix rendering error for using useSearchParams by wrapping with suspense

--- nextjs-jsonplaceholder-app/app/Post_Details/page.js
+++ nextjs-jsonplaceholder-app/app/Post_Details/page.js
@@ -3,21 +3,24 @@
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import { fetching } from "./fetching"; 
+import { Suspense } from "react";
+
 
 function Display() {
-    
-    const router = useRouter(); // useRouter hook for back nagivation
+    const router = useRouter(); // useRouter hook for back navigation
     const searchParams = useSearchParams(); // useSearchParams hook to get the id
     const id = searchParams.get("id"); 
-    const content = fetching(id); // fetching post details from the apI
+    const content = fetching(id); // fetching post details from the API
 
+    if (!content) return (
+        <p className="text-center text-white">Loading...</p>
+    );
 
-    
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
-            <div className="max-w-lg bg-white p-6 rounded-lg shadow-1g">
-                <h1 className="text-2xl font-bold text-gray-800  text-center">{content?.title}</h1>
-                <p className="mt-7 text-gray-700 text-center font-serif">{content?.body}</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div className="max-w-lg bg-white p-6 rounded-lg shadow-lg">
+                <h1 className="text-2xl font-bold text-gray-800 text-center">{content.title}</h1>
+                <p className="mt-7 text-gray-700 text-center font-serif">{content.body}</p>
                 
                 <button  
                     className="mt-10 px-6 py-2 bg-black text-white font-bold rounded-full shadow-md transition"
@@ -30,4 +33,12 @@ function Display() {
     );
 }
 
-export default Display;
+export default function Page() {
+    // added suspense for using  useSearchParams, to avoid reendering error
+    return (
+        <Suspense fallback={
+        <p className="text-center text-white">Loading...</p>}>
+            <Display />
+        </Suspense>
+    );
+}
